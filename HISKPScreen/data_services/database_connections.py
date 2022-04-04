7c7994c0d1996849880e7bd688a555c915446e1b
Kai Habermann <kai.habermann@gmx.net>
2022-04-04 13:13:33 +0200

    temp

--- HISKPScreen/data_services/database_connections.py
+++ HISKPScreen/data_services/database_connections.py
@@ -1,10 +1,14 @@
 import psycopg2 as pg
 import psycopg2.extras
+import time
 
 def get_conn_and_cur(database="main_db"):
-    query = "dbname='%s' user='postgres' host='db' password='postgres'"%database
-    # print(query)
-    conn = pg.connect(query)
-
-    return  conn, conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    for _ in range (10):
+        try:
+            query = "dbname='%s' user='postgres' host='db' password='postgres'"%database
+            # print(query)
+            conn = pg.connect(query)
+            return  conn, conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        except:
+            time.sleep(5)
 
