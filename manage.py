95c0eb8d7c5593d43cf32f3bf859f7d37e388ad0
Kai Habermann <kai.habermann@gmx.net>
2022-04-04 13:31:23 +0200

    jetzte aber

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import imp
 import os
 import sys
 from HISKPScreen.data_services.setup import database_setup
-
+import time
 
 def main():
     """Run administrative tasks."""
@@ -21,4 +22,5 @@ def main():
 
 
 if __name__ == '__main__':
+    time.sleep(10)
     main()
